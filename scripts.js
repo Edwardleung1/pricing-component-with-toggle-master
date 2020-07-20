4a6fad62b529cd2d19369bbf87aac35285a99686
Edward Leung <edward_leung_12@hotmail.com>
2020-07-20 19:32:41 +0100

    update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const toggle = document.getElementById("toggle");
 const flexy = document.getElementById("flexy");
 
+// by default it shows the annually prices first
 toggle.addEventListener("change", (e) => {
   flexy.classList.toggle("show-monthly");
 });
